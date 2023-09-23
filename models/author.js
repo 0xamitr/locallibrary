5661defd8e4587b0978814ab91d72b5209d6eb8a
amit <amit@Xubuntu.myguest.virtualbox.org>
2023-09-23 21:04:12 +0530

    implemented view engines

--- models/author.js
+++ models/author.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const AuthorSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
@@ -17,7 +18,6 @@ AuthorSchema.virtual("name").get(function () {
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
   }
-
   return fullname;
 });
 
@@ -27,5 +27,12 @@ AuthorSchema.virtual("url").get(function () {
   return `/catalog/author/${this._id}`;
 });
 
+AuthorSchema.virtual("date_birth").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("date_death").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
--- routes/catalog.js
+++ routes/catalog.js
@@ -39,62 +39,62 @@ router.get("/books", book_controller.book_list);
 /// AUTHOR ROUTES ///
 
 // GET request for creating Author. NOTE This must come before route for id (i.e. display author).
-// router.get("/author/create", author_controller.author_create_get);
+router.get("/author/create", author_controller.author_create_get);
 
-// // POST request for creating Author.
-// router.post("/author/create", author_controller.author_create_post);
+// POST request for creating Author.
+router.post("/author/create", author_controller.author_create_post);
 
-// // GET request to delete Author.
-// router.get("/author/:id/delete", author_controller.author_delete_get);
+// GET request to delete Author.
+router.get("/author/:id/delete", author_controller.author_delete_get);
 
-// // POST request to delete Author.
-// router.post("/author/:id/delete", author_controller.author_delete_post);
+// POST request to delete Author.
+router.post("/author/:id/delete", author_controller.author_delete_post);
 
-// // GET request to update Author.
-// router.get("/author/:id/update", author_controller.author_update_get);
+// GET request to update Author.
+router.get("/author/:id/update", author_controller.author_update_get);
 
-// // POST request to update Author.
-// router.post("/author/:id/update", author_controller.author_update_post);
+// POST request to update Author.
+router.post("/author/:id/update", author_controller.author_update_post);
 
-// // GET request for one Author.
-// router.get("/author/:id", author_controller.author_detail);
+// GET request for one Author.
+router.get("/author/:id", author_controller.author_detail);
 
-// // GET request for list of all Authors.
-// router.get("/authors", author_controller.author_list);
+// GET request for list of all Authors.
+router.get("/authors", author_controller.author_list);
 
-// /// GENRE ROUTES ///
+/// GENRE ROUTES ///
 
-// // GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
-// router.get("/genre/create", genre_controller.genre_create_get);
+// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
+router.get("/genre/create", genre_controller.genre_create_get);
 
-// //POST request for creating Genre.
-// router.post("/genre/create", genre_controller.genre_create_post);
+//POST request for creating Genre.
+router.post("/genre/create", genre_controller.genre_create_post);
 
-// // GET request to delete Genre.
-// router.get("/genre/:id/delete", genre_controller.genre_delete_get);
+// GET request to delete Genre.
+router.get("/genre/:id/delete", genre_controller.genre_delete_get);
 
-// // POST request to delete Genre.
-// router.post("/genre/:id/delete", genre_controller.genre_delete_post);
+// POST request to delete Genre.
+router.post("/genre/:id/delete", genre_controller.genre_delete_post);
 
-// // GET request to update Genre.
-// router.get("/genre/:id/update", genre_controller.genre_update_get);
+// GET request to update Genre.
+router.get("/genre/:id/update", genre_controller.genre_update_get);
 
-// // POST request to update Genre.
-// router.post("/genre/:id/update", genre_controller.genre_update_post);
+// POST request to update Genre.
+router.post("/genre/:id/update", genre_controller.genre_update_post);
 
-// // GET request for one Genre.
-// router.get("/genre/:id", genre_controller.genre_detail);
+// GET request for one Genre.
+router.get("/genre/:id", genre_controller.genre_detail);
 
-// // GET request for list of all Genre.
-// router.get("/genres", genre_controller.genre_list);
+// GET request for list of all Genre.
+router.get("/genres", genre_controller.genre_list);
 
-// /// BOOKINSTANCE ROUTES ///
+/// BOOKINSTANCE ROUTES ///
 
-// // GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id).
-// router.get(
-//   "/bookinstance/create",
-//   book_instance_controller.bookinstance_create_get,
-// );
+// GET request for creating a BookInstance. NOTE This must come before route that displays BookInstance (uses id).
+router.get(
+  "/bookinstance/create",
+  book_instance_controller.bookinstance_create_get,
+);
 
 // POST request for creating BookInstance.
 router.post(
