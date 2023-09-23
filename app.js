799ae1830c83812af7ee862bdeed84c929062047
amit <amit@Xubuntu.myguest.virtualbox.org>
2023-09-23 14:30:51 +0530

    added routes with placeholders

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require("./routes/catalog");
 const wiki = require("./wiki.js");
 
 var app = express();
@@ -24,7 +25,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use("/wiki", wiki);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
