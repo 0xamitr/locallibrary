e28a900f4ea422546e866f8d473eb1d46564572f
0xamitr <amitrathore.aaa@gmail.com>
2023-09-30 05:45:32 +0530

    test

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mongoose.set("strictQuery", false);
 
 const dev_db_url =
   "mongodb+srv://username:AOTARY061J4QEwUD@cluster0.xhe3oqr.mongodb.net/local_library?retryWrites=true&w=majority";
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const mongoDB = process.env.MONGODB_URI || dev_db_url; 
 
 main().catch((err) => console.log(err));
 async function main() {
